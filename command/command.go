fae8a282f41d38e7c926bdd12112b13811d90b45
hakobe <hakobe@gmail.com>
2014-05-19 18:06:02 +0900

    Separates Generator definitions to arch specific file

--- command/command.go
+++ command/command.go
@@ -9,10 +9,7 @@ import (
 	"github.com/mackerelio/mackerel-agent/agent"
 	"github.com/mackerelio/mackerel-agent/logging"
 	"github.com/mackerelio/mackerel-agent/mackerel"
-	"github.com/mackerelio/mackerel-agent/metrics"
-	metricsLinux "github.com/mackerelio/mackerel-agent/metrics/linux"
 	"github.com/mackerelio/mackerel-agent/spec"
-	specLinux "github.com/mackerelio/mackerel-agent/spec/linux"
 )
 
 var logger = logging.GetLogger("command")
@@ -130,33 +127,6 @@ func loop(ag *agent.Agent, api *mackerel.API, host *mackerel.Host) {
 			postQueue <- creatingValues
 		}
 	}
-}
-
-func metaGenerators() []spec.Generator {
-	return  []spec.Generator{
-		&specLinux.KernelGenerator{},
-		&specLinux.CPUGenerator{},
-		&specLinux.MemoryGenerator{},
-		&specLinux.BlockDeviceGenerator{},
-		&specLinux.FilesystemGenerator{},
-		&specLinux.InterfaceGenerator{},
-	}
-}
-
-func metricsGenerators(config mackerel.Config) []metrics.Generator {
-	generators := []metrics.Generator{
-		&metricsLinux.Loadavg5Generator{},
-		&metricsLinux.CpuusageGenerator{Interval: 60},
-		&metricsLinux.MemoryGenerator{},
-		&metricsLinux.UptimeGenerator{},
-		&metricsLinux.InterfaceGenerator{Interval: 60},
-		&metricsLinux.DiskGenerator{Interval: 60},
-	}
-	for _, pluginConfig := range config.Plugin["metrics"] {
-		generators = append(generators, &metricsLinux.PluginGenerator{pluginConfig})
-	}
-
-	return generators
 }
 
 func Run(config mackerel.Config) {
---
+++ command/comment_linux.go
@@ -0,0 +1,36 @@
+package command
+
+import (
+	"github.com/mackerelio/mackerel-agent/mackerel"
+	"github.com/mackerelio/mackerel-agent/metrics"
+	metricsLinux "github.com/mackerelio/mackerel-agent/metrics/linux"
+	"github.com/mackerelio/mackerel-agent/spec"
+	specLinux "github.com/mackerelio/mackerel-agent/spec/linux"
+)
+
+func metaGenerators() []spec.Generator {
+	return  []spec.Generator{
+		&specLinux.KernelGenerator{},
+		&specLinux.CPUGenerator{},
+		&specLinux.MemoryGenerator{},
+		&specLinux.BlockDeviceGenerator{},
+		&specLinux.FilesystemGenerator{},
+		&specLinux.InterfaceGenerator{},
+	}
+}
+
+func metricsGenerators(config mackerel.Config) []metrics.Generator {
+	generators := []metrics.Generator{
+		&metricsLinux.Loadavg5Generator{},
+		&metricsLinux.CpuusageGenerator{Interval: 60},
+		&metricsLinux.MemoryGenerator{},
+		&metricsLinux.UptimeGenerator{},
+		&metricsLinux.InterfaceGenerator{Interval: 60},
+		&metricsLinux.DiskGenerator{Interval: 60},
+	}
+	for _, pluginConfig := range config.Plugin["metrics"] {
+		generators = append(generators, &metricsLinux.PluginGenerator{pluginConfig})
+	}
+
+	return generators
+}
