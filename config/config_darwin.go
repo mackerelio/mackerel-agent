709a4f320f93b11e181f7a8b586832557255b4a2
motemen <motemen@gmail.com>
2014-11-14 15:20:21 +0900

    correct comments

--- config/config_darwin.go
+++ config/config_darwin.go
@@ -16,7 +16,7 @@ var DefaultConfig = &Config{
 	Connection: ConnectionConfig{
 		Post_Metrics_Dequeue_Delay_Seconds: 30,     // Check the metric values queue for each half minutes
 		Post_Metrics_Retry_Delay_Seconds:   60,     // Wait a minute before retrying metric value posts
-		Post_Metrics_Retry_Max:             60,     // Retry up to 60 times (5min * 60 = 5hrs)
+		Post_Metrics_Retry_Max:             60,     // Retry up to 60 times (30s * 60 = 30min)
 		Post_Metrics_Buffer_Size:           6 * 60, // Keep metric values of 6 hours span in the queue
 	},
 }
