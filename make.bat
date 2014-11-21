8770afdd890b99e4ea119c066f4afe7005e78db9
daiksy <daiksy@hatena.ne.jp>
2014-11-21 08:34:49 +0000

    add "make.bat" for build on Windows

---
+++ make.bat
@@ -0,0 +1,7 @@
+FOR /F "usebackq" %%w IN (`git rev-parse --short HEAD`) DO SET COMMIT=%%w
+
+FOR /F "usebackq" %%w IN (`git describe --tags --abbrev^=0`) DO SET VERSION=%%w
+
+set VERSION=%VERSION:v=%
+
+go build -o build/mackerel-agent -ldflags="-X github.com/mackerelio/mackerel-agent/version.GITCOMMIT %COMMIT% -X github.com/mackerelio/mackerel-agent/version.VERSION %VERSION% " github.com/mackerelio/mackerel-agent
