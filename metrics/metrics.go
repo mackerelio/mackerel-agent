af2b01b48848f1eaf138b6541039fbf261dae47b
Songmu <y.songmu@gmail.com>
2014-12-28 23:56:17 +0900

    metrics/metrics.go:5:6: exported type Values should have comment or be unexported metrics/metrics.go:7:1: exported method Values.Merge should have comment or be unexported metrics/metrics.go:13:6: exported type Generator should have comment or be unexported metrics/metrics.go:17:6: exported type PluginGenerator should have comment or be unexported metrics/plugin.go:43:5: don't use ALL_CAPS in Go names; use CamelCase metrics/plugin.go:43:5: exported var PLUGIN_PREFIX should have comment or be unexported metrics/plugin.go:47:1: exported function NewPluginGenerator should have comment or be unexported

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -2,18 +2,22 @@ package metrics
 
 import "github.com/mackerelio/mackerel-agent/mackerel"
 
+// Values XXX
 type Values map[string]float64
 
+// Merge XXX
 func (vs *Values) Merge(other Values) {
 	for k, v := range (map[string]float64)(other) {
 		(*vs)[k] = v
 	}
 }
 
+// Generator XXX
 type Generator interface {
 	Generate() (Values, error)
 }
 
+// PluginGenerator XXX
 type PluginGenerator interface {
 	Generate() (Values, error)
 	PrepareGraphDefs(api *mackerel.API) ([]mackerel.CreateGraphDefsPayload, error)
