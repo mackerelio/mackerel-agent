48ae8b6d43db604b8341cddf2340a8f53e7270c3
Hiroaki Abe <hiroaki0404@gmail.com>
2015-01-04 10:57:07 +0900

    Supporting interface for darwin.

---
+++ metrics/darwin/interface_test.go
@@ -0,0 +1,33 @@
+// +build darwin
+
+package darwin
+
+import "testing"
+
+func TestInterfaceGenerator(t *testing.T) {
+	g := &InterfaceGenerator{1}
+	values, err := g.Generate()
+	if err != nil {
+		t.Error("should not raise error")
+	}
+
+	metrics := []string{
+		"rxBytes", "txBytes",
+	}
+
+	for _, metric := range metrics {
+		if value, ok := values["interface.en0."+metric+".delta"]; !ok {
+			t.Errorf("Value for interface.en0.%s.delta should be collected", metric)
+		} else {
+			t.Logf("Interface en0 '%s' delta collected: %+v", metric, value)
+		}
+	}
+
+	for _, metric := range metrics {
+		if _, ok := values["interface.lo."+metric+".delta"]; ok {
+			t.Errorf("Value for interface.lo.%s should NOT be collected", metric)
+		} else {
+			t.Logf("Interface lo '%s' NOT collected", metric)
+		}
+	}
+}
