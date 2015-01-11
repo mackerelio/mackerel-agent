d4b6ce6ebc519882b1d8eff11dc3d323f6231509
Songmu <y.songmu@gmail.com>
2015-01-11 15:46:39 +0900

    add comment to tool/cover.sh

--- tool/cover.sh
+++ tool/cover.sh
@@ -1,4 +1,8 @@
 #/bin/sh
+# Calculate coverage
+# run this and you can see coverage reports as following
+#   % go tool cover -html .profile.cov
+
 set -e
 
 prof=${1:-".profile.cov"}
@@ -12,6 +16,8 @@ cleanup() {
 }
 trap cleanup INT QUIT TERM EXIT
 
+# cmd/cover doesn't support -coverprofile for multiple packages, so run tests in each dir.
+# ref. https://github.com/mattn/goveralls/issues/20
 for dir in $(find . -not -path '*/[._]*' -type d); do
   if ls $dir/*.go > /dev/null 2>&1; then
     tmpprof=$dir/profile.tmp
