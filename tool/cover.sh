c3c241b594cfae43e5fff834a5046a677f5f46a1
Songmu <y.songmu@gmail.com>
2015-01-11 15:10:20 +0900

    remove double brackets syntax

--- tool/cover.sh
+++ tool/cover.sh
@@ -1,9 +1,9 @@
-#/bin/bash
+#/bin/sh
 prof=${1:-".profile.cov"}
 echo "mode: count" > $prof
 
 cleanup() {
-  if [[ $tmpprof != "" && -f $tmpprof ]]; then
+  if [ $tmpprof != "" && -f $tmpprof ]; then
     rm -f $tmpprof
   fi
   exit
@@ -14,7 +14,7 @@ for dir in $(find . -not -path '*/[._]*' -type d); do
   if ls $dir/*.go > /dev/null 2>&1; then
     tmpprof=$dir/profile.tmp
     go test -covermode=count -coverprofile=$tmpprof $dir
-    if [[ -f $tmpprof ]]; then
+    if [ -f $tmpprof ]; then
       cat $tmpprof | tail -n +2 >> $prof
       rm $tmpprof
     fi
