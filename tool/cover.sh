9b5b760625d5fa6d4e690a450c4675cd31aa1b18
Songmu <y.songmu@gmail.com>
2015-01-13 11:02:59 +0900

    refactor cover.sh

--- tool/cover.sh
+++ tool/cover.sh
@@ -2,7 +2,6 @@
 # Calculate coverage
 # run this and you can see coverage reports as following
 #   % go tool cover -html .profile.cov
-
 set -e
 
 prof=${1:-".profile.cov"}
@@ -18,8 +17,9 @@ trap cleanup INT QUIT TERM EXIT
 
 # cmd/cover doesn't support -coverprofile for multiple packages, so run tests in each dir.
 # ref. https://github.com/mattn/goveralls/issues/20
+gopath1=$(echo $GOPATH | cut -d: -f1)
 for pkg in $(go list ./...); do
-  tmpprof=$(echo $GOPATH | cut -d: -f1)/src/$pkg/profile.tmp
+  tmpprof=$gopath1/src/$pkg/profile.tmp
   go test -covermode=count -coverprofile=$tmpprof $pkg
   if [ -f $tmpprof ]; then
     cat $tmpprof | tail -n +2 >> $prof
