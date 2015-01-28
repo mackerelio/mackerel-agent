4fe96cf69b5936619f2d82a7a53bdb3d327b29b6
mechairoi <ttsujikawa@gmail.com>
2015-01-28 20:09:27 +0900

    Change DefaultConfig pathes on windows

--- config/config_windows.go
+++ config/config_windows.go
@@ -1,11 +1,33 @@
 package config
 
+import (
+	"log"
+	"path/filepath"
+	"syscall"
+	"unsafe"
+)
+
+func execdirInit() string {
+	var (
+		kernel32              = syscall.NewLazyDLL("kernel32")
+		procGetModuleFileName = kernel32.NewProc("GetModuleFileNameW")
+	)
+	var wpath [syscall.MAX_PATH]uint16
+	r1, _, err := procGetModuleFileName.Call(0, uintptr(unsafe.Pointer(&wpath[0])), uintptr(len(wpath)))
+	if r1 == 0 {
+		log.Fatal(err)
+	}
+	return syscall.UTF16ToString(wpath[:])
+}
+
+var execdir = filepath.Dir(execdirInit())
+
 // The default configuration for windows
 var DefaultConfig = &Config{
 	Apibase:  "https://mackerel.io",
-	Root:     ".",
-	Pidfile:  "mackerel-agent.pid",
-	Conffile: "mackerel-agent.conf",
+	Root:     execdir,
+	Pidfile:  filepath.Join(execdir, "mackerel-agent.pid"),
+	Conffile: filepath.Join(execdir, "mackerel-agent.conf"),
 	Roles:    []string{},
 	Verbose:  false,
 	Connection: ConnectionConfig{
