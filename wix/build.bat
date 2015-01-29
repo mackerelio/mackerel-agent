bddd8861ab073eae0f2ef2212ec9dbe2c02883a4
mechairoi <ttsujikawa@gmail.com>
2015-01-29 16:28:54 +0900

    rename to *_windows for build on linux

--- wix/build.bat
+++ wix/build.bat
@@ -7,8 +7,8 @@ pushd %0\..
 go get github.com/mackerelio/mackerel-agent/wix/wrapper
 go get github.com/mackerelio/mackerel-agent/wix/replace
 
-go build -o ..\build\wrapper.exe wrapper\wrapper.go
-go build -o ..\build\replace.exe replace\replace.go
+go build -o ..\build\wrapper.exe wrapper\wrapper_windows.go
+go build -o ..\build\replace.exe replace\replace_windows.go
 
 "%WIX%bin\candle.exe" mackerel-agent.wxs
 "%WIX%bin\light.exe" -out "..\build\mackerel-agent.msi" mackerel-agent.wixobj
