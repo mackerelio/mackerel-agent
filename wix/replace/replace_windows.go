59df433c4f34f2d8150b80a45b800d51c5d178bb
mechairoi <ttsujikawa@gmail.com>
2015-02-03 18:22:11 +0900

    Rename variable to avoid built-in function name

--- wix/replace/replace_windows.go
+++ wix/replace/replace_windows.go
@@ -12,15 +12,15 @@ func main() {
 		log.Fatal("Usage: replace [filename] [old string] [new string]")
 	}
 	file := os.Args[1]
-	old := os.Args[2]
-	new := os.Args[3]
+	oldStr := os.Args[2]
+	newStr := os.Args[3]
 
 	content, err := ioutil.ReadFile(file)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = ioutil.WriteFile(file, []byte(strings.Replace(string(content), old, new, -1)), 0644)
+	err = ioutil.WriteFile(file, []byte(strings.Replace(string(content), oldStr, newStr, -1)), 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
