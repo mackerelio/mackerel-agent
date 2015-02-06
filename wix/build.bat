d452ee0ad368e755f0b3c92babdc0371cd22475b
mechairoi <ttsujikawa@gmail.com>
2015-02-06 14:56:12 +0900

    Fill version in wxs file from tag

--- wix/build.bat
+++ wix/build.bat
@@ -10,6 +10,14 @@ go get github.com/mackerelio/mackerel-agent/wix/replace
 go build -o ..\build\wrapper.exe wrapper\wrapper_windows.go
 go build -o ..\build\replace.exe replace\replace_windows.go
 
+
+
+FOR /F "usebackq" %%w IN (`git describe --tags --abbrev^=0`) DO SET VERSION=%%w
+set VERSION=%VERSION:v=%
+FOR /F "tokens=1 delims=-+" %%w IN ('ECHO %%VERSION%%') DO SET VERSION=%%w
+
+..\build\replace.exe mackerel-agent.wxs.template mackerel-agent.wxs "___VERSION___" "%VERSION%"
+
 "%WIX%bin\candle.exe" mackerel-agent.wxs
 "%WIX%bin\light.exe" -out "..\build\mackerel-agent.msi" mackerel-agent.wixobj
 
