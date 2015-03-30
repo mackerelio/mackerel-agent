a8702fe825ea911439b5c91427918644f75b3c6e
Songmu <y.songmu@gmail.com>
2015-03-30 15:57:49 +0900

    timeoutDuration and timeoutKillAfter make private for now

--- util/util.go
+++ util/util.go
@@ -10,17 +10,18 @@ import (
 	"github.com/Songmu/timeout"
 )
 
-// TimeoutDuration is option of `Runcommand()` set timeout limit of command execution.
-var TimeoutDuration = 30 * time.Second
-// TimeoutKillAfter is option of `RunCommand()` set waiting limit to `kill -kill` after terminating the command.
-var TimeoutKillAfter = 10 * time.Second
+// timeoutDuration is option of `Runcommand()` set timeout limit of command execution.
+var timeoutDuration = 30 * time.Second
+
+// timeoutKillAfter is option of `RunCommand()` set waiting limit to `kill -kill` after terminating the command.
+var timeoutKillAfter = 10 * time.Second
 
 // RunCommand runs command (in one string) and returns stdout, stderr strings.
 func RunCommand(command string) (string, string, error) {
 	tio := &timeout.Timeout{
 		Cmd:       exec.Command("/bin/sh", "-c", command),
-		Duration:  TimeoutDuration,
-		KillAfter: TimeoutKillAfter,
+		Duration:  timeoutDuration,
+		KillAfter: timeoutKillAfter,
 	}
 	exitStatus, stdout, stderr, err := tio.Run()
 
