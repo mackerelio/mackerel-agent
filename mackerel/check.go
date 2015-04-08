404d7fcce002c22979b7089cddbcc9988f669813
motemen <motemen@gmail.com>
2015-04-08 12:56:05 +0900

    check API

---
+++ mackerel/check.go
@@ -0,0 +1,47 @@
+package mackerel
+
+import (
+	"encoding/json"
+
+	"github.com/mackerelio/mackerel-agent/checks"
+)
+
+type monitoringChecksPayload struct {
+	Checks []*checkReport `json:"checks"`
+}
+
+type checkReport struct {
+	Target     monitorTargetHost `json:"target"`
+	Name       string            `json:"name"`
+	Status     checks.Status     `json:"status"`
+	Message    string            `json:"message"`
+	OccurredAt Time              `json:"occurredAt"`
+}
+
+type monitorTargetHost struct {
+	HostID string
+}
+
+// implements json.Marshaler
+func (h monitorTargetHost) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string]string{
+		"type":   "host",
+		"hostId": h.HostID,
+	})
+}
+
+func (api *API) ReportCheckMonitors(hostID string, reports []*checks.Report) error {
+	payload := &monitoringChecksPayload{
+		Checks: make([]*checkReport, len(reports)),
+	}
+	for i, report := range reports {
+		payload.Checks[i] = &checkReport{
+			Target:     monitorTargetHost{HostID: hostID},
+			Name:       report.Name,
+			Status:     report.Status,
+			Message:    report.Message,
+			OccurredAt: Time(report.OccurredAt),
+		}
+	}
+	return api.postJSON("/api/v0/monitoring/checks", payload, nil)
+}
