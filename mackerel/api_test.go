bc2545eb90349857fdad73c8eb759895698fc161
Songmu <y.songmu@gmail.com>
2015-05-31 22:04:10 +0900

    TestCreateGraphDefs

--- mackerel/api_test.go
+++ mackerel/api_test.go
@@ -407,3 +407,77 @@ func TestPostHostMetricValues(t *testing.T) {
 		t.Error("err shoud be nil but: ", err)
 	}
 }
+
+func TestCreateGraphDefs(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		if req.URL.Path != "/api/v0/graph-defs/create" {
+			t.Error("request URL should be /api/v0/graph-defs/create but :", req.URL.Path)
+		}
+
+		if req.Method != "POST" {
+			t.Error("request method should be GET but :", req.Method)
+		}
+		body, _ := ioutil.ReadAll(req.Body)
+
+		var datas []struct {
+			Name        string                         `json:"name"`
+			DisplayName string                         `json:"displayName"`
+			Unit        string                         `json:"unit"`
+			Metrics     []CreateGraphDefsPayloadMetric `json:"metrics"`
+		}
+
+		err := json.Unmarshal(body, &datas)
+		if err != nil {
+			t.Fatal("request body should be decoded as json", string(body))
+		}
+		data := datas[0]
+
+		if data.Name != "mackerel" {
+			t.Errorf("request sends json including name but: %s", data.Name)
+		}
+		if data.DisplayName != "HorseMackerel" {
+			t.Errorf("request sends json including DisplayName but: %s", data.Name)
+		}
+		if !reflect.DeepEqual(
+			data.Metrics[0],
+			CreateGraphDefsPayloadMetric{
+				Name:        "saba1",
+				DisplayName: "aji1",
+				IsStacked:   false,
+			},
+		) {
+			t.Error("request sends json including GraphDefsMetric but: ", data.Metrics[0])
+		}
+		respJSON, _ := json.Marshal(map[string]string{
+			"result": "OK",
+		})
+		res.Header()["Content-Type"] = []string{"application/json"}
+		fmt.Fprint(res, string(respJSON))
+	}))
+	defer ts.Close()
+
+	api, _ := NewAPI(ts.URL, "dummy-key", false)
+	err := api.CreateGraphDefs([]CreateGraphDefsPayload{
+		CreateGraphDefsPayload{
+			Name:        "mackerel",
+			DisplayName: "HorseMackerel",
+			Unit:        "percentage",
+			Metrics: []CreateGraphDefsPayloadMetric{
+				CreateGraphDefsPayloadMetric{
+					Name:        "saba1",
+					DisplayName: "aji1",
+					IsStacked:   false,
+				},
+				CreateGraphDefsPayloadMetric{
+					Name:        "saba2",
+					DisplayName: "aji2",
+					IsStacked:   false,
+				},
+			},
+		},
+	})
+
+	if err != nil {
+		t.Error("err shoud be nil but: ", err)
+	}
+}
