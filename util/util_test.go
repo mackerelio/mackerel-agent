71cedf413cfe6f626c3801b1863147b8534c4915
Songmu <y.songmu@gmail.com>
2015-06-06 02:03:41 +0900

    add util_test.go

---
+++ util/util_test.go
@@ -0,0 +1,41 @@
+// +build linux darwin freebsd
+
+package util
+
+import (
+	"testing"
+	"time"
+)
+
+func init() {
+	timeoutDuration = 1 * time.Second
+}
+
+func TestRunCommand(t *testing.T) {
+	stdout, stderr, exitCode, err := RunCommand("echo 1")
+	if stdout != "1\n" {
+		t.Errorf("stdout shoud be 1")
+	}
+	if stderr != "" {
+		t.Errorf("stderr shoud be empty")
+	}
+	if exitCode != 0 {
+		t.Errorf("exitCode should be zero")
+	}
+	if err != nil {
+		t.Error("err should be nil but:", err)
+	}
+}
+
+func TestRunCommandWithTimeout(t *testing.T) {
+	stdout, stderr, _, err := RunCommand("sleep 2")
+	if stdout != "" {
+		t.Errorf("stdout shoud be empty")
+	}
+	if stderr != "" {
+		t.Errorf("stderr shoud be empty")
+	}
+	if err == nil {
+		t.Error("err should have error but nil")
+	}
+}
