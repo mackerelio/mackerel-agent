cbc00b5762dc4065ec02bb3170533e77d8bc1832
Tomohiro Nishimura <tomohiro68@gmail.com>
2015-06-23 17:28:10 +0900

    fix for 1.3.3

--- command/command.go
+++ command/command.go
@@ -174,7 +174,7 @@ func loop(c *context, termCh chan struct{}) int {
 
 	// fan-out termCh
 	go func() {
-		for range termCh {
+		for _ = range termCh {
 			termCheckerCh <- struct{}{}
 			termMetricsCh <- struct{}{}
 		}
@@ -426,7 +426,7 @@ func runCheckersLoop(c *context, termCheckerCh <-chan struct{}, quit <-chan stru
 	} else {
 		// consume termCheckerCh
 		go func() {
-			for range termCheckerCh {
+			for _ = range termCheckerCh {
 			}
 		}()
 	}
