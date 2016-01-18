b8bb04f03605b3b5568373b4e7beae708b322c1a
mechairoi <ttsujikawa@gmail.com>
2016-01-18 14:46:29 +0900

    add a test for max_check_attempts

--- config/config_test.go
+++ config/config_test.go
@@ -28,6 +28,7 @@ type = "metric"
 [plugin.checks.heartbeat]
 command = "heartbeat.sh"
 notification_interval = 60
+max_check_attempts = 3
 `
 
 func TestLoadConfig(t *testing.T) {
@@ -169,6 +170,9 @@ func TestLoadConfigFile(t *testing.T) {
 	}
 	if *checks.NotificationInterval != 60 {
 		t.Error("notification_interval should be 60")
+	}
+	if *checks.MaxCheckAttempts != 3 {
+		t.Error("max_check_attempts should be 3")
 	}
 }
 
