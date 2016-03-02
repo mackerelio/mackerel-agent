64269c07a2884b78ec470d2c2e0e0d42ee99f491
Songmu <y.songmu@gmail.com>
2016-03-02 13:23:36 +0900

    add test for checker timeout. ref #197

--- checks/checker_test.go
+++ checks/checker_test.go
@@ -1,9 +1,12 @@
 package checks
 
 import (
+	"runtime"
 	"testing"
+	"time"
 
 	"github.com/mackerelio/mackerel-agent/config"
+	"github.com/mackerelio/mackerel-agent/util"
 )
 
 func TestChecker_Check(t *testing.T) {
@@ -18,12 +21,6 @@ func TestChecker_Check(t *testing.T) {
 			Command: "go run testdata/exit.go -code 1 -message something_is_going_wrong",
 		},
 	}
-
-	// checkerTimeout := Checker{
-	// 	Config: config.PluginConfig{
-	// 		Command: "sleep 35",
-	// 	},
-	// }
 
 	{
 		report, err := checkerOK.Check()
@@ -51,16 +48,34 @@ func TestChecker_Check(t *testing.T) {
 		}
 	}
 
-	// {
-	// 	report, err := checkerTimeout.Check()
-	// 	if err != nil {
-	// 		t.Errorf("err should be nil: %v", err)
-	// 	}
-	// 	if report.Status != StatusUnknown {
-	// 		t.Errorf("status should be UNKNOWN: %v", report.Status)
-	// 	}
-	// 	if report.Message != "command timed out" {
-	// 		t.Errorf("wrong message: %q", report.Message)
-	// 	}
-	// }
+}
+
+func TestChecker_CheckTimeout(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skipf("skip check timeout test in windows")
+	}
+	origTimeoutDur := util.TimeoutDuration
+	defer func() {
+		util.TimeoutDuration = origTimeoutDur
+	}()
+	util.TimeoutDuration = 1 * time.Second
+
+	checkerTimeout := Checker{
+		Config: config.PluginConfig{
+			Command: "sleep 2",
+		},
+	}
+
+	{
+		report, err := checkerTimeout.Check()
+		if err != nil {
+			t.Errorf("err should be nil: %v", err)
+		}
+		if report.Status != StatusUnknown {
+			t.Errorf("status should be UNKNOWN: %v", report.Status)
+		}
+		if report.Message != "command timed out" {
+			t.Errorf("wrong message: %q", report.Message)
+		}
+	}
 }
