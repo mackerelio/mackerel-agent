eeaee876782d78044958e83f0a1847bdd02d8225
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-09-12 23:38:25 +0900

    fixed wix\build.bat

--- wix/build.bat
+++ wix/build.bat
@@ -16,7 +16,7 @@ go get -d github.com/mackerelio/go-check-plugins/...
 go get -d github.com/mackerelio/mackerel-agent-plugins/...
 
 FOR /F %%w in (.\wix\pluginlist.txt) DO (
-  echo go build -o ..\build\%%~nw.exe %%w
+  go build -o build\%%~nw.exe %%w
 )
 
 CD %~dp0
