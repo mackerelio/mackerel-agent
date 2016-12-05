434b1f59d49d71258b20103ed65dfe3af1f5630e
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-12-06 01:14:28 +0900

    write comment simply.

--- wix/wrapper/install.go
+++ wix/wrapper/install.go
@@ -1,6 +1,4 @@
-// Copyright 2012 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// This is based on https://github.com/golang/sys/tree/master/windows/svc
 
 // +build windows
 
