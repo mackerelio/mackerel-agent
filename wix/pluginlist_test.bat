4e62fbaea657f284f8ca0e2d00ac9a003c7a61ec
Asato Wakisaka <asato.wakisaka@gmail.com>
2017-01-13 00:24:46 +0900

    oops

--- wix/pluginlist_test.bat
+++ wix/pluginlist_test.bat
@@ -4,7 +4,7 @@ go get -d github.com/mackerelio/go-check-plugins/...
 go get -d github.com/mackerelio/mackerel-agent-plugins/...
 
 FOR /F %%w in (.\wix\pluginlist.txt) DO (
-  go list %%W
+  go list %%w
   if not "%ERRORLEVEL%" == "0" (
     exit /b 1
   )
