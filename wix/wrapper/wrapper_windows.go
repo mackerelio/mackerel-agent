de5ad06f5938931863f8a6b8722474ef8172b096
daiksy <daiksy@hatena.ne.jp>
2017-01-24 15:47:29 +0900

    remove InstallAsEventCreate

--- wix/wrapper/wrapper_windows.go
+++ wix/wrapper/wrapper_windows.go
@@ -45,10 +45,6 @@ func main() {
 		return
 	}
 
-	err := eventlog.InstallAsEventCreate(name, eventlog.Error|eventlog.Warning|eventlog.Info)
-	if err != nil {
-		log.Fatal("SetupEventLogSource() failed: %s", err)
-	}
 	elog, err := eventlog.Open(name)
 	if err != nil {
 		log.Fatal(err.Error())
