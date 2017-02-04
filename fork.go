56cc7364c2745db0fc60ade7e795a8d71e5bc7dc
Songmu <y.songmu@gmail.com>
2017-02-04 23:00:33 +0900

    add signal handler

--- fork.go
+++ fork.go
@@ -3,6 +3,8 @@ package main
 import (
 	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -18,7 +20,17 @@ func handleFork(prog string, argv []string) error {
 	_ = startAt
 	cmd.Start()
 
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGHUP)
+	go func() {
+		for sig := range c {
+			if sig == syscall.SIGHUP {
+				// reload agent
+				cmd.Process.Signal(sig)
+			} else {
+				cmd.Process.Signal(sig)
+			}
+		}
+	}()
 	return cmd.Wait()
-	// c := make(chan os.Signal)
-	// signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGHUP)
 }
