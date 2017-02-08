383db3f6afa5cd5ba500d1291374f48aa4056471
Asato Wakisaka <asato.wakisaka@gmail.com>
2017-02-08 17:53:49 +0900

    image name was wrong

--- _tools/packaging/rpmbuild-docker.sh
+++ _tools/packaging/rpmbuild-docker.sh
@@ -3,4 +3,4 @@
 set -e
 set -x
 
-docker run --rm -v "$PWD":/workspace -v "$PWD/rpmbuild":/rpmbuild astj-mackerel-packager-beta:$RPMBUILD_DOCKER_TAG $@
+docker run --rm -v "$PWD":/workspace -v "$PWD/rpmbuild":/rpmbuild astj/mackerel-agent-packager-beta:$RPMBUILD_DOCKER_TAG $@
