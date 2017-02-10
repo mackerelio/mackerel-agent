862320d14cd187d3fc81c38c479a1c141c528c72
Songmu <y.songmu@gmail.com>
2017-02-11 00:52:02 +0900

    add TestSuperviseReload

--- supervisor_test.go
+++ supervisor_test.go
@@ -5,7 +5,9 @@ package main
 import (
 	"os"
 	"os/exec"
+	"syscall"
 	"testing"
+	"time"
 )
 
 func init() {
@@ -25,5 +27,41 @@ func TestSupervise(t *testing.T) {
 
 	if err == nil {
 		t.Errorf("something went wrong")
+	}
+}
+
+func TestSuperviseReload(t *testing.T) {
+	sv := &supervisor{
+		prog: "testdata/stub-agent",
+	}
+	sv.start()
+	ch := make(chan os.Signal)
+	go sv.handleSignal(ch)
+	done := make(chan error)
+	go func() {
+		done <- sv.wait()
+	}()
+	origPid := sv.cmd.Process.Pid
+	ch <- syscall.SIGHUP
+	time.Sleep(time.Second)
+	newPid := sv.cmd.Process.Pid
+	if origPid == newPid {
+		t.Errorf("reload failed")
+	}
+	if existsPid(origPid) {
+		t.Errorf("original process doesn't terminated")
+	}
+
+	ch <- syscall.SIGTERM
+	err := <-done
+	if err == nil {
+		t.Errorf("something went wrong")
+	}
+	if newPid != sv.cmd.Process.Pid {
+		t.Errorf("something went wrong")
+	}
+
+	if existsPid(newPid) {
+		t.Errorf("child process doesn't terminated")
 	}
 }
