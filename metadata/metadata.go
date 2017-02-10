60b12a04b2f3ec51e3021f6c3f5017165631d5af
itchyny <itchyny@hatena.ne.jp>
2017-02-10 18:28:33 +0900

    implement Fetch to execute command fetch metadata

--- metadata/metadata.go
+++ metadata/metadata.go
@@ -1,11 +1,37 @@
 package metadata
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/mackerelio/mackerel-agent/config"
+	"github.com/mackerelio/mackerel-agent/logging"
 )
+
+var logger = logging.GetLogger("metadata")
 
 // MetadataGenerator generates metadata
 type MetadataGenerator struct {
 	Name   string
 	Config *config.MetadataPlugin
+}
+
+// Fetch invokes the command and returns the result
+func (g *MetadataGenerator) Fetch() (string, error) {
+	message, stderr, exitCode, err := g.Config.Run()
+
+	if err != nil {
+		logger.Warningf("Error occurred while executing a metadata plugin %q: %s", g.Name, err.Error())
+		return "", err
+	}
+
+	if stderr != "" {
+		logger.Warningf("Metadata generator %q outputs stderr: %s", g.Name, stderr)
+	}
+
+	if exitCode != 0 {
+		return "", errors.New(fmt.Sprintf("Metadata plugin command exits with: %d", exitCode))
+	}
+
+	return message, nil
 }
