d3ca67e6e7570011e7e55706af705bcb05e6f963
itchyny <itchyny@hatena.ne.jp>
2017-02-14 11:10:18 +0900

    add tests for Fetch() of metadata plugin generator

--- metadata/metadata_test.go
+++ metadata/metadata_test.go
@@ -7,6 +7,80 @@ import (
 	"github.com/mackerelio/mackerel-agent/config"
 )
 
+func TestMetadataGenerator(t *testing.T) {
+	tests := []struct {
+		command string
+		message string
+		err     bool
+	}{
+		{
+			command: `go run testdata/json.go -exit-code 0 -message "{}"`,
+			message: `{}`,
+			err:     false,
+		},
+		{
+			command: `go run testdata/json.go -exit-code 1 -message "{}"`,
+			message: ``,
+			err:     true,
+		},
+		{
+			command: `go run testdata/json.go -exit-code 0 -message '{"example": "message"}'`,
+			message: `{"example": "message"}`,
+			err:     false,
+		},
+		{
+			command: `go run testdata/json.go -exit-code 0 -message '{"example": message"}'`,
+			message: ``,
+			err:     true,
+		},
+		{
+			command: `go run testdata/json.go -exit-code 0 -message '"foobar"'`,
+			message: `"foobar"`,
+			err:     false,
+		},
+		{
+			command: `go run testdata/json.go -exit-code 0 -message foobar`,
+			message: ``,
+			err:     true,
+		},
+		{
+			command: `go run testdata/json.go -exit-code 0 -message 16777216`,
+			message: `16777216`,
+			err:     false,
+		},
+		{
+			command: `go run testdata/json.go -exit-code 0 -message true`,
+			message: `true`,
+			err:     false,
+		},
+		{
+			command: `go run testdata/json.go -exit-code 0 -message null`,
+			message: `null`,
+			err:     false,
+		},
+	}
+	for _, test := range tests {
+		g := Generator{
+			Config: &config.MetadataPlugin{
+				Command: test.command,
+			},
+		}
+		message, err := g.Fetch()
+		if err != nil {
+			if !test.err {
+				t.Errorf("error occurred unexpectedly on command %q", test.command)
+			}
+		} else {
+			if test.err {
+				t.Errorf("error did not occurr but error expected on command %q", test.command)
+			}
+			if message != test.message {
+				t.Errorf("message should be %q but got %q", test.message, message)
+			}
+		}
+	}
+}
+
 func TestMetadataGeneratorInterval(t *testing.T) {
 	tests := []struct {
 		interval *int32
