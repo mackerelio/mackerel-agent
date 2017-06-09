1a55e1fbca0a2176c1d27b1b7214b2b8b758b561
Songmu <y.songmu@gmail.com>
2017-06-09 15:39:48 +0900

    update build*.bat

--- build.bat
+++ build.bat
@@ -2,10 +2,4 @@ echo on
 
 FOR /F "usebackq" %%w IN (`git rev-parse --short HEAD`) DO SET COMMIT=%%w
 
-FOR /F "usebackq" %%w IN (`git describe --tags --abbrev^=0`) DO SET VERSION=%%w
-
-set VERSION=%VERSION:v=%
-
-echo %VERSION%
-
-go build -o build/mackerel-agent.exe -ldflags="-X main.gitcommit=%COMMIT% -X main.version=%VERSION% " github.com/mackerelio/mackerel-agent
+go build -o build/mackerel-agent.exe -ldflags="-X main.gitcommit=%COMMIT% " github.com/mackerelio/mackerel-agent
