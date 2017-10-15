ae10f28ad0e46890dad03410fa607bb1efe07e05
itchyny <itchyny@hatena.ne.jp>
2017-10-15 09:52:46 +0900

    use the external library to collect network traffic on darwin

--- metrics/darwin/interface_test.go
+++ metrics/darwin/interface_test.go
@@ -14,9 +14,7 @@ func TestInterfaceGenerator(t *testing.T) {
 		t.Errorf("should not raise error: %v", err)
 	}
 
-	metrics := []string{
-		"rxBytes", "txBytes",
-	}
+	metrics := []string{"rxBytes", "txBytes"}
 
 	for _, metric := range metrics {
 		if value, ok := values["interface.en0."+metric+".delta"]; !ok {
@@ -27,10 +25,10 @@ func TestInterfaceGenerator(t *testing.T) {
 	}
 
 	for _, metric := range metrics {
-		if _, ok := values["interface.lo."+metric+".delta"]; ok {
-			t.Errorf("Value for interface.lo.%s should NOT be collected", metric)
+		if _, ok := values["interface.lo0."+metric+".delta"]; ok {
+			t.Errorf("Value for interface.lo0.%s should NOT be collected", metric)
 		} else {
-			t.Logf("Interface lo '%s' NOT collected", metric)
+			t.Logf("Interface lo0 '%s' NOT collected", metric)
 		}
 	}
 }
