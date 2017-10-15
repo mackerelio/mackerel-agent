9448a4a644f2b74c7cadfe8a9031c1a400ae1ac5
itchyny <itchyny@hatena.ne.jp>
2017-10-15 09:51:06 +0900

    use the external library to collect loadavg5 on darwin

--- metrics/darwin/loadavg5.go
+++ metrics/darwin/loadavg5.go
@@ -3,10 +3,7 @@
 package darwin
 
 import (
-	"os/exec"
-	"strconv"
-	"strings"
-
+	"github.com/mackerelio/go-osstat/loadavg"
 	"github.com/mackerelio/golib/logging"
 	"github.com/mackerelio/mackerel-agent/metrics"
 )
@@ -17,32 +14,12 @@ type Loadavg5Generator struct {
 
 var loadavg5Logger = logging.GetLogger("metrics.loadavg5")
 
-// % sysctl -n vm.loadavg
-// { 2.26 2.08 2.00 }
-
-// Generate XXX
+// Generate load averages
 func (g *Loadavg5Generator) Generate() (metrics.Values, error) {
-	outputBytes, err := exec.Command("sysctl", "-n", "vm.loadavg").Output()
+	loadavgs, err := loadavg.Get()
 	if err != nil {
-		loadavg5Logger.Errorf("Failed to run sysctl -n vm.loadavg: %s", err)
+		loadavg5Logger.Errorf("%s", err)
 		return nil, err
 	}
-
-	output := string(outputBytes)
-
-	// fields will be "{", <loadavg1>, <loadavg5>, <loadavg15>, "}"
-	fields := strings.Fields(output)
-
-	if len(fields) != 5 || fields[0] != "{" || fields[len(fields)-1] != "}" {
-		loadavg5Logger.Errorf("sysctl -n vm.loadavg result malformed: %s", output)
-		return nil, err
-	}
-
-	loadavg5, err := strconv.ParseFloat(fields[2], 64)
-	if err != nil {
-		loadavg5Logger.Errorf("Failed to parse loadavg5 string: %s", err)
-		return nil, err
-	}
-
-	return metrics.Values{"loadavg5": loadavg5}, nil
+	return metrics.Values{"loadavg5": loadavgs.Loadavg5}, nil
 }
