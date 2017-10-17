41f710bdcd635dcda7d627e28338f0024b2d4815
itchyny <itchyny@hatena.ne.jp>
2017-10-17 19:01:29 +0900

    use the external library to collect cpu statistics on linux

--- metrics/linux/cpuusage.go
+++ metrics/linux/cpuusage.go
@@ -3,13 +3,9 @@
 package linux
 
 import (
-	"bufio"
-	"os"
-	"regexp"
-	"strconv"
-	"strings"
 	"time"
 
+	"github.com/mackerelio/go-osstat/cpu"
 	"github.com/mackerelio/golib/logging"
 	"github.com/mackerelio/mackerel-agent/metrics"
 )
@@ -22,20 +18,6 @@ collect CPU usage
 metric = "user", "nice", "system", "idle", "iowait", "irq", "softirq", "steal", "guest"
 
 graph: stacks `cpu.{metric}.percentage`
-
-cat /proc/stat sample: {{{
-	cpu  7792253 5479 4851396 18056319678 127239 0 146818 2383839
-	cpu0 5385397 1412 1970781 4509432750 103260 0 136689 876389
-	cpu1 641247 1361 782257 4516019361 7247 0 2403 452803
-	cpu2 652342 1366 617100 4516172153 7762 0 2447 453509
-	cpu3 1113265 1339 1481257 4514695413 8968 0 5278 601135
-	intr 6664031039 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 3682869251 40969382 60 304 40427429 141 567698585 39988217 145 500771676 67725387 95 1170166889 187 33636967 83463 519692861 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-	ctxt 14007527061
-	btime 1349954031
-	processes 60807520
-	procs_running 1
-	procs_blocked 0
-}}}
 */
 
 // CPUUsageGenerator XXX
@@ -43,95 +25,46 @@ type CPUUsageGenerator struct {
 	Interval time.Duration
 }
 
-// In additions these metrics, collect *.percentage metrics
-var cpuUsageMetricNames = []string{
-	"cpu.user", "cpu.nice", "cpu.system", "cpu.idle", "cpu.iowait",
-	"cpu.irq", "cpu.softirq", "cpu.steal", "cpu.guest",
-}
-
-var cpuNumberPattern = regexp.MustCompile(`^cpu\d+\s`)
-
 var cpuUsageLogger = logging.GetLogger("metrics.cpuUsage")
 
 // Generate XXX
 func (g *CPUUsageGenerator) Generate() (metrics.Values, error) {
-	prevValues, prevTotal, _, err := g.collectProcStatValues()
+	before, err := g.collectProcStatValues()
 	if err != nil {
 		return nil, err
 	}
 
 	time.Sleep(g.Interval)
 
-	currValues, currTotal, cpuCount, err := g.collectProcStatValues()
+	after, err := g.collectProcStatValues()
 	if err != nil {
 		return nil, err
 	}
 
-	ret := make(map[string]float64)
-	for i, name := range cpuUsageMetricNames {
-		// Values in /proc/stat differ in Linux kernel versions.
-		// Not all metrics in cpuUsageMetricNames can be retrieved.
-		// ref: `man 5 proc`
-		if i >= len(currValues) || i >= len(prevValues) {
-			break
-		}
+	totalDiff := float64(after.Total - before.Total)
+	cpuCount := float64(after.CPUCount)
 
-		// percentage of increased amount of CPU time
-		ret[name+".percentage"] = (currValues[i] - prevValues[i]) * 100.0 * float64(cpuCount) / (currTotal - prevTotal)
+	ret := map[string]float64{
+		"cpu.user.percentage":    float64((after.User-after.Guest)-(before.User-before.Guest)) * cpuCount * 100.0 / totalDiff,
+		"cpu.nice.percentage":    float64(after.Nice-before.Nice) * cpuCount * 100.0 / totalDiff,
+		"cpu.system.percentage":  float64(after.System-before.System) * cpuCount * 100.0 / totalDiff,
+		"cpu.idle.percentage":    float64(after.Idle-before.Idle) * cpuCount * 100.0 / totalDiff,
+		"cpu.iowait.percentage":  float64(after.Iowait-before.Iowait) * cpuCount * 100.0 / totalDiff,
+		"cpu.irq.percentage":     float64(after.Irq-before.Irq) * cpuCount * 100.0 / totalDiff,
+		"cpu.softirq.percentage": float64(after.Softirq-before.Softirq) * cpuCount * 100.0 / totalDiff,
+		"cpu.steal.percentage":   float64(after.Steal-before.Steal) * cpuCount * 100.0 / totalDiff,
+		"cpu.guest.percentage":   float64(after.Guest-before.Guest) * cpuCount * 100.0 / totalDiff,
+		// "cpu.guest_nice.percentage": float64(after.GuestNice - before.GuestNice) * cpuCount * 100.0 / totalDiff,
 	}
-
 	return metrics.Values(ret), nil
 }
 
 // returns values corresponding to cpuUsageMetricNames, those total and the number of CPUs
-func (g *CPUUsageGenerator) collectProcStatValues() ([]float64, float64, uint, error) {
-	file, err := os.Open("/proc/stat")
+func (g *CPUUsageGenerator) collectProcStatValues() (*cpu.Stats, error) {
+	cpu, err := cpu.Get()
 	if err != nil {
-		cpuUsageLogger.Errorf("Failed (skip these metrics): %s", err)
-		return nil, 0, 0, err
+		cpuUsageLogger.Errorf("failed to get cpu statistics: %s", err)
+		return nil, err
 	}
-
-	lineScanner := bufio.NewScanner(bufio.NewReader(file))
-
-	var cols []string
-	var cpuCount uint
-	firstLine := true
-
-	for lineScanner.Scan() {
-		line := lineScanner.Text()
-
-		if firstLine {
-			// first line contains total values of all CPUs
-			cols = strings.Fields(lineScanner.Text())[1:]
-			firstLine = false
-		} else if cpuNumberPattern.MatchString(line) {
-			// number of cores
-			cpuCount++
-		} else {
-			break
-		}
-	}
-
-	values := make([]float64, len(cols))
-
-	var totalValues float64
-	for i, strValue := range cols {
-		values[i], err = strconv.ParseFloat(strValue, 64)
-		if err != nil {
-			cpuUsageLogger.Errorf("Failed to parse cpuUsage metrics (skip these metrics): %s", err)
-			return nil, 0, 0, err
-		}
-		totalValues += values[i]
-	}
-
-	// Since cpustat[CPUTIME_USER] includes cpustat[CPUTIME_GUEST], subtract the duplicated values from total.
-	// https://github.com/torvalds/linux/blob/4ec9f7a18/kernel/sched/cputime.c#L151-L158
-	// https://github.com/mackerelio/mackerel-agent/issues/419
-	if len(values) >= 9 {
-		totalValues -= values[8]
-		// Also, subtract guest from user.
-		values[0] -= values[8]
-	}
-
-	return values, totalValues, cpuCount, nil
+	return cpu, nil
 }
