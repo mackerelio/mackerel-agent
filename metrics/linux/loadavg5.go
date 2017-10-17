487b2da2cbc65e4c44ab1039c971da6593566dda
itchyny <itchyny@hatena.ne.jp>
2017-10-17 18:27:42 +0900

    use the external library to collect loadavg statistics on linux

--- metrics/linux/loadavg5.go
+++ metrics/linux/loadavg5.go
@@ -3,21 +3,13 @@
 package linux
 
 import (
-	"io/ioutil"
-	"strconv"
-	"strings"
-
+	"github.com/mackerelio/go-osstat/loadavg"
 	"github.com/mackerelio/golib/logging"
 	"github.com/mackerelio/mackerel-agent/metrics"
 )
 
-/*
-collect load average
-
-`loadavg5`: load average per 5 minutes retrieved from /proc/loadavg
-
-graph: `loadavg5`
-*/
+// loadavg5
+//   - loadavg5: load average per 5 minutes
 
 // Loadavg5Generator XXX
 type Loadavg5Generator struct {
@@ -25,21 +17,12 @@ type Loadavg5Generator struct {
 
 var loadavg5Logger = logging.GetLogger("metrics.loadavg5")
 
-// Generate XXX
+// Generate load averages
 func (g *Loadavg5Generator) Generate() (metrics.Values, error) {
-	contentbytes, err := ioutil.ReadFile("/proc/loadavg")
+	loadavgs, err := loadavg.Get()
 	if err != nil {
-		loadavg5Logger.Errorf("Failed (skip these metrics): %s", err)
+		loadavg5Logger.Errorf("%s", err)
 		return nil, err
 	}
-	content := string(contentbytes)
-	cols := strings.Split(content, " ")
-
-	f, err := strconv.ParseFloat(cols[1], 64)
-	if err != nil {
-		loadavg5Logger.Errorf("Failed to parse loadavg5 metrics (skip these metrics): %s", err)
-		return nil, err
-	}
-
-	return metrics.Values{"loadavg5": f}, nil
+	return metrics.Values{"loadavg5": loadavgs.Loadavg5}, nil
 }
--- metrics/linux/loadavg5_test.go
+++ metrics/linux/loadavg5_test.go
@@ -5,9 +5,19 @@ package linux
 import "testing"
 
 func TestLoadAvg5Generate(t *testing.T) {
-	_, err := (&Loadavg5Generator{}).Generate()
+	g := &Loadavg5Generator{}
+	values, err := g.Generate()
 
 	if err != nil {
-		t.Errorf("something went wrong")
+		t.Errorf("error should be nil but got: %s", err)
 	}
+
+	metricName := []string{"loadavg5"}
+	for _, n := range metricName {
+		if _, ok := values[n]; !ok {
+			t.Errorf("loadavg5 metrics should have '%s': %v", n, values)
+		}
+	}
+
+	t.Logf("loadavg5 metrics: %+v", values)
 }
