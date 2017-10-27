8ac9af35b8583591949b62c0614ae77cadb5f2d8
itchyny <itchyny@hatena.ne.jp>
2017-10-27 16:46:00 +0900

    Commonize loadavg5 generators

--- command/command_darwin.go
+++ command/command_darwin.go
@@ -23,7 +23,7 @@ func interfaceGenerator() spec.InterfaceGenerator {
 
 func metricsGenerators(conf *config.Config) []metrics.Generator {
 	generators := []metrics.Generator{
-		&metricsDarwin.Loadavg5Generator{},
+		&metrics.Loadavg5Generator{},
 		&metricsDarwin.CPUUsageGenerator{},
 		&metricsDarwin.MemoryGenerator{},
 		&metrics.FilesystemGenerator{IgnoreRegexp: conf.Filesystems.Ignore.Regexp, UseMountpoint: conf.Filesystems.UseMountpoint},
--- metrics/loadavg5.go
+++ metrics/loadavg5.go
@@ -1,11 +1,10 @@
-// +build linux
+// +build !windows
 
-package linux
+package metrics
 
 import (
 	"github.com/mackerelio/go-osstat/loadavg"
 	"github.com/mackerelio/golib/logging"
-	"github.com/mackerelio/mackerel-agent/metrics"
 )
 
 // loadavg5
@@ -18,11 +17,11 @@ type Loadavg5Generator struct {
 var loadavg5Logger = logging.GetLogger("metrics.loadavg5")
 
 // Generate load averages
-func (g *Loadavg5Generator) Generate() (metrics.Values, error) {
+func (g *Loadavg5Generator) Generate() (Values, error) {
 	loadavgs, err := loadavg.Get()
 	if err != nil {
 		loadavg5Logger.Errorf("%s", err)
 		return nil, err
 	}
-	return metrics.Values{"loadavg5": loadavgs.Loadavg5}, nil
+	return Values{"loadavg5": loadavgs.Loadavg5}, nil
 }
