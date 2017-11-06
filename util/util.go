a7b0bbe6ca2e8ce5d657f18c4c74b782b92d436e
Yasuhisa Yoshida <syou6162@gmail.com>
2017-11-06 11:45:22 +0900

    use %v

--- util/util.go
+++ util/util.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
-	"strings"
 	"time"
 
 	"github.com/Songmu/timeout"
@@ -57,7 +56,7 @@ func RunCommandArgs(cmdArgs []string, user string, env []string) (stdout, stderr
 		err = fmt.Errorf("command timed out")
 	}
 	if err != nil {
-		utilLogger.Errorf("RunCommand error command: %s, error: %s", strings.Join(cmdArgs, " "), err.Error())
+		utilLogger.Errorf("RunCommand error command: %v, error: %s", cmdArgs, err.Error())
 	}
 	return stdout, stderr, exitStatus.GetChildExitCode(), err
 }
