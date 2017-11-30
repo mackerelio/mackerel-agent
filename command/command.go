5d2725045c47497eebd4cef24fe8d5c7eb3b8cb9
Asato Wakisaka <asato.wakisaka@gmail.com>
2017-11-30 15:12:46 +0900

    app itself has Config

--- command/command.go
+++ command/command.go
@@ -342,7 +342,7 @@ func loop(app *App, termCh chan struct{}) error {
 
 func updateHostSpecsLoop(app *App, quit chan struct{}) {
 	for {
-		app.UpdateHostSpecs(app.Config)
+		app.UpdateHostSpecs()
 		select {
 		case <-quit:
 			return
@@ -555,10 +555,10 @@ func fillUpSpecMeta(meta map[string]interface{}, ver, rev string) map[string]int
 }
 
 // UpdateHostSpecs updates the host information that is already registered on Mackerel.
-func (app *App) UpdateHostSpecs(conf *config.Config) {
+func (app *App) UpdateHostSpecs() {
 	logger.Debugf("Updating host specs...")
 
-	hostname, meta, interfaces, customIdentifier, err := collectHostSpecs(conf)
+	hostname, meta, interfaces, customIdentifier, err := collectHostSpecs(app.Config)
 	if err != nil {
 		logger.Errorf("While collecting host specs: %s", err)
 		return
