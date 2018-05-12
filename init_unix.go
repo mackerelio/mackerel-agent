8bcd52bee8f7afdc2f25e9e03b4063461793ca2a
speter <speter.go1@gmail.com>
2018-05-12 06:30:38 +0200

    more robust PATH handling

--- init_unix.go
+++ init_unix.go
@@ -2,10 +2,22 @@
 
 package main
 
-import "os"
+import (
+	"os"
+
+	"gopkg.in/pathlist.v0"
+	"gopkg.in/pathlist.v0/env"
+)
 
 func init() {
-	os.Setenv("PATH", "/sbin:/usr/sbin:/bin:/usr/bin:"+os.Getenv("PATH"))
+	prependTo := func(list pathlist.List, dir string) pathlist.List {
+		return pathlist.Must(pathlist.PrependTo(list, dir))
+	}
+	list := prependTo(env.Path(), "/usr/bin")
+	list = prependTo(list, "/bin")
+	list = prependTo(list, "/usr/sbin")
+	list = prependTo(list, "/sbin")
+	env.SetPath(list)
 	// prevent changing outputs of some command, e.g. ifconfig.
 	os.Setenv("LANG", "C")
 	os.Setenv("LC_ALL", "C")
--- init_windows.go
+++ init_windows.go
@@ -5,11 +5,13 @@ package main
 import (
 	"os"
 	"path/filepath"
+
+	"gopkg.in/pathlist.v0"
+	"gopkg.in/pathlist.v0/env"
 )
 
 func init() {
 	if p, err := os.Executable(); err == nil {
-		os.Setenv("PATH", filepath.Dir(p)+
-			string(filepath.ListSeparator)+os.Getenv("PATH"))
+		env.SetPath(pathlist.Must(pathlist.PrependTo(env.Path(), filepath.Dir(p))))
 	}
 }
