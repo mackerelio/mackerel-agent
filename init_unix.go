cc85d5e3ba1fc8535bd8310c59c7eceda9b94f64
speter <speter.go1@gmail.com>
2018-05-12 23:31:54 +0200

    more robust PATH handling: simplify

--- init_unix.go
+++ init_unix.go
@@ -10,14 +10,8 @@ import (
 )
 
 func init() {
-	prependTo := func(list pathlist.List, dir string) pathlist.List {
-		return pathlist.Must(pathlist.PrependTo(list, dir))
-	}
-	list := prependTo(env.Path(), "/usr/bin")
-	list = prependTo(list, "/bin")
-	list = prependTo(list, "/usr/sbin")
-	list = prependTo(list, "/sbin")
-	env.SetPath(list)
+	env.SetPath(pathlist.Must(pathlist.PrependTo(env.Path(),
+		"/sbin", "/usr/sbin", "/bin", "/usr/bin")))
 	// prevent changing outputs of some command, e.g. ifconfig.
 	os.Setenv("LANG", "C")
 	os.Setenv("LC_ALL", "C")
