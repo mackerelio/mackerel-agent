4ec4a309dc902539c5e90a231ee18c2d6d6d0615
itchyny <itchyny@hatena.ne.jp>
2018-08-08 01:22:33 +0900

    use HostMeta of mackerel-client-go in HostSpec

--- command/command.go
+++ command/command.go
@@ -576,9 +576,9 @@ func collectHostSpecs(conf *config.Config, ameta *AgentMeta) (mackerel.HostSpec,
 		return mackerel.HostSpec{}, fmt.Errorf("failed to collect interfaces: %s", err.Error())
 	}
 
-	meta["agent-version"] = ameta.Version
-	meta["agent-revision"] = ameta.Revision
-	meta["agent-name"] = buildUA(ameta.Version, ameta.Revision)
+	meta.AgentVersion = ameta.Version
+	meta.AgentRevision = ameta.Revision
+	meta.AgentName = buildUA(ameta.Version, ameta.Revision)
 
 	checks := []mackerel.CheckConfig{}
 	for name, checkPlugin := range conf.CheckPlugins {
