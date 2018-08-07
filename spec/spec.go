4fcfc72920025e8a8e409cc78c9b41e9bf7d031e
itchyny <itchyny@hatena.ne.jp>
2018-08-08 01:13:44 +0900

    use type switch to set to HostMeta in spec.Collect

--- spec/spec.go
+++ spec/spec.go
@@ -2,26 +2,40 @@ package spec
 
 import (
 	"github.com/mackerelio/golib/logging"
+	"github.com/mackerelio/mackerel-client-go"
 )
 
 var logger = logging.GetLogger("spec")
 
 // Generator interface for generating spec values
 type Generator interface {
-	Key() string
 	Generate() (interface{}, error)
 }
 
 // Collect spec values
-func Collect(specGenerators []Generator) map[string]interface{} {
-	specs := make(map[string]interface{})
+func Collect(specGenerators []Generator) mackerel.HostMeta {
+	var specs mackerel.HostMeta
 	for _, g := range specGenerators {
 		value, err := g.Generate()
 		if err != nil {
 			logger.Warningf("Failed to collect meta in %T (skip this spec): %s", g, err.Error())
 			continue
 		}
-		specs[g.Key()] = value
+		switch v := value.(type) {
+		case mackerel.BlockDevice:
+			specs.BlockDevice = v
+		case mackerel.CPU:
+			specs.CPU = v
+		case mackerel.FileSystem:
+			specs.Filesystem = v
+		case mackerel.Kernel:
+			specs.Kernel = v
+		case mackerel.Memory:
+			specs.Memory = v
+		case *mackerel.Cloud:
+			specs.Cloud = v
+		default:
+		}
 	}
 	return specs
 }
