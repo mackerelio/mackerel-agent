adde8fc025b2d484430d0b51ce982824087d07fa
itchyny <itchyny@hatena.ne.jp>
2018-11-20 23:22:15 +0900

    update comment in metrics/linux/memory.go

--- metrics/linux/memory.go
+++ metrics/linux/memory.go
@@ -15,9 +15,9 @@ MemoryGenerator collect memory usage
 
 metric = "total", "free", "buffers", "cached", "active", "inactive", "swap_cached", "swap_total", "swap_free"
 
-Metrics "used" is calculated here like (total - free - buffers - cached) or (total - available) or for ease.
-(total - available) is chosen when `MemAvailableEnabled` is enabled.
-This calculation may be going to be done in server side in the future.
+Metrics "used" is calculated by
+  (total - available) when MemAvailable is available in /proc/meminfo and
+  (total - free - buffers - cached) otherwise
 
 graph: stacks `memory.{metric}`
 */
