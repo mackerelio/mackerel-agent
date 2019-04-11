6c687956cbf942c755ebcf91b839bbb7cbd0690e
itchyny <itchyny@hatena.ne.jp>
2019-04-11 14:32:16 +0900

    check command name of saved pid in existsPid on Darwin

--- pidfile/pid_darwin.go
+++ pidfile/pid_darwin.go
@@ -1,16 +1,28 @@
 package pidfile
 
 import (
+	"bytes"
 	"fmt"
-	"io/ioutil"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
-func existsPid(pid int) bool {
-	cmd := exec.Command("/usr/sbin/lsof", "-p", fmt.Sprint(pid))
-	cmd.Stdout = ioutil.Discard
-	cmd.Stderr = ioutil.Discard
+func existsPid(pid int) (b bool) {
+	cmd := exec.Command("/bin/ps", "-o", "command=", fmt.Sprint(pid))
+
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
 
 	err := cmd.Run()
-	return err == nil
+	if err != nil {
+		return false
+	}
+
+	out := stdout.String()
+	if i := strings.IndexRune(out, ' '); i > 0 {
+		out = out[:i]
+	}
+	return filepath.Base(out) == filepath.Base(os.Args[0])
 }
