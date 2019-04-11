0fa6ef05ec645cbfee10f50fd163d921c72a7475
itchyny <itchyny@hatena.ne.jp>
2019-04-11 15:30:49 +0900

    remove TestCreate_mutex test

--- pidfile/pidfile_test.go
+++ pidfile/pidfile_test.go
@@ -3,7 +3,6 @@
 package pidfile
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -36,20 +35,6 @@ func TestCreate(t *testing.T) {
 	err = Create(pidfile)
 	if err != nil {
 		t.Errorf("When the content of pidfile is the same as own pid, the error should be nil, but: %s", err.Error())
-	}
-}
-
-func TestCreate_mutex(t *testing.T) {
-	dir, err := ioutil.TempDir("", "")
-	if err != nil {
-		t.Fatalf("failed to create tempdir")
-	}
-	defer os.RemoveAll(dir)
-	pidfile := filepath.Join(dir, "pidfile")
-	ioutil.WriteFile(pidfile, []byte(fmt.Sprintf("%d", os.Getppid())), 0644)
-	err = Create(pidfile)
-	if err == nil {
-		t.Errorf("if pidfile exists and its process is running, an error should be returned, but successfully overwriting the pidfile unintentionally")
 	}
 }
 
