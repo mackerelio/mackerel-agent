65906226c87f11a359ffd1149b730f5201005a0b
itchyny <itchyny@hatena.ne.jp>
2019-07-24 16:30:08 +0900

    add test for pidfile.ExistsPid that it checks the process command name

--- pidfile/pid_test.go
+++ pidfile/pid_test.go
@@ -3,8 +3,10 @@
 package pidfile
 
 import (
+	"context"
 	"math"
 	"os"
+	"os/exec"
 	"testing"
 )
 
@@ -14,5 +16,30 @@ func TestExistsPid(t *testing.T) {
 	}
 	if ExistsPid(math.MaxInt32) {
 		t.Errorf("something went wrong")
+	}
+}
+
+func TestExistsPid_CheckProcessName(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	cmd := exec.CommandContext(ctx, "sleep", "10")
+	cmd.Stdout = os.Stdout
+	err := cmd.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
+	pid := cmd.Process.Pid
+
+	origArg0 := os.Args[0]
+	defer func() { os.Args[0] = origArg0 }()
+
+	os.Args[0] = "/bin/go-test"
+	if existsPid(pid) {
+		t.Errorf("existsPid should return false when os.Args[0] is %q and pid: %v", os.Args[0], pid)
+	}
+
+	os.Args[0] = "/bin/sleep" // note that only the base name is checked
+	if !existsPid(pid) {
+		t.Errorf("existsPid should return true when os.Args[0] is %q and pid: %v", os.Args[0], pid)
 	}
 }
