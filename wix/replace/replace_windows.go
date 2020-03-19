52de0476245b9d26d776a590e9749627dfd1487f
lufia <lufia@lufia.org>
2020-03-19 17:38:30 +0900

    replace: check fallback directory

--- wix/replace/replace_windows.go
+++ wix/replace/replace_windows.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -16,17 +17,34 @@ func main() {
 	oldStr := os.Args[3]
 	newStr := os.Args[4]
 
+	if isExists(outFile) {
+		return
+	}
+	if dir := os.Getenv("MACKEREL_CONFIG_FALLBACK"); dir != "" {
+		file := filepath.Join(dir, "mackerel-agent.conf")
+		if isExists(file) {
+			return
+		}
+	}
+
 	content, err := ioutil.ReadFile(inFile)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	_, err = os.Stat(outFile)
-	outFileIsExists := err == nil
-	if !(outFileIsExists) {
-		err = ioutil.WriteFile(outFile, []byte(strings.Replace(string(content), oldStr, newStr, -1)), 0644)
-		if err != nil {
-			log.Fatal(err)
-		}
+	err = ioutil.WriteFile(outFile, []byte(strings.Replace(string(content), oldStr, newStr, -1)), 0644)
+	if err != nil {
+		log.Fatal(err)
 	}
+}
+
+func isExists(file string) bool {
+	f, err := os.Open(file)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+		return true // file is existed; but can't read it.
+	}
+	f.Close()
+	return true
 }
