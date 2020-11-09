9e361737c1a2163612e676ca71593a3b4378f32d
Asato Wakisaka <asato.wakisaka@gmail.com>
2020-11-10 00:23:52 +0900

    rename

---
+++ mackerel/mock/gen.go
@@ -0,0 +1,3 @@
+//go:generate mockgen -package=mockmackerel -destination=mock_api.go github.com/mackerelio/mackerel-agent/mackerel API
+
+package mockmackerel
--- mackerel/mock_api/gen.go
+++
@@ -1,3 +0,0 @@
-//go:generate mockgen -package=mockMackerel -destination=mock_api.go github.com/mackerelio/mackerel-agent/mackerel API
-
-package mockMackerel
