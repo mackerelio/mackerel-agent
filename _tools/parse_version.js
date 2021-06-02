5a8df10ced630ac649575870f22433d8f84d2bb4
yseto <yseto@users.noreply.github.com>
2021-06-02 11:54:41 +0900

    modify refer to review comments.

--- _tools/parse_version.js
+++ _tools/parse_version.js
@@ -1,16 +1,14 @@
 const fs = require("fs/promises");
 
-async function parse() {
+(async function main() {
   const content = await fs.readFile("version.go", "utf8");
   const regex = new RegExp(`^const +version *= *"(?<version>.+)?"`, "m");
   const version = regex.exec(content)?.groups?.version;
   if (version === undefined) {
-    process.on("exit", function() {
-      process.exit(1);
-    });
-  } else {
-    console.log("::set-output name=VERSION::" + version);
+    throw new Error("version is undefined.");
   }
-}
-
-parse();
+  console.log("::set-output name=VERSION::" + version);
+})().catch(error => {
+  console.error(error);
+  process.exit(1);
+});
