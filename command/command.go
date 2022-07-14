0accbe866a84afdb2643ceb205c7df875e2ad347
sugy <sugy.kz@gmail.com>
2022-07-14 15:28:54 +0900

    Added the delay log output condition.
    Delay affects only when the report is larger than checkReportMaxSize.

--- command/command.go
+++ command/command.go
@@ -536,7 +536,9 @@ func runCheckersLoop(ctx context.Context, app *App, termCheckerCh <-chan struct{
 		// Extend the delay when there are lots of reports
 		if len(reports) > len(app.Agent.Checkers)*2 {
 			reportCheckDelay = reportCheckDelaySecondsMax
-			logger.Warningf("RunCheckerLoop: Extend the delay to %d seconds for every %d reports. There are %d reports.", reportCheckDelay, checkReportMaxSize, len(reports))
+			if len(reports) > checkReportMaxSize {
+				logger.Warningf("RunCheckerLoop: Extend the delay to %d seconds for every %d reports. There are %d reports.", reportCheckDelay, checkReportMaxSize, len(reports))
+			}
 		}
 
 		// "" means no CustomIdentifier, which means the host running this agent itself.
