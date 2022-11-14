c2c4ce4c4c17a70fd7ddc53e47c8331d0f91fc74
wafuwafu13 <wafuwafu1313@gmail.com>
2022-11-14 11:17:14 +0900

    refactor test key name

--- config/validate_test.go
+++ config/validate_test.go
@@ -21,16 +21,16 @@ use_mntpoint = true
 command = "test command"
 env = { FOO = "BAR" }
 
-[plugin.metric.1]
+[plugin.metric.incorrect1]
 command = "test command"
 
-[plugin.check.1]
+[plugin.check.incorrect2]
 command = "test command"
 
-[plugin.check.2]
+[plugin.check.incorrect3]
 command = "test command"
 
-[plugins.check.1]
+[plugins.check.incorrect4]
 command = "test command"
 
 [plugin.metrics.correct]
@@ -39,18 +39,18 @@ command = "test command"
 [plugin.checks.correct]
 command = "test command"
 
-[plugin.metrics.1]
+[plugin.metrics.incorrect5]
 command = "test command"
 action = { command = "test command", use = "test user", env = { TEST_KEY = "VALUE_1" } }
 
-[plugin.metrics.2]
+[plugin.metrics.incorrect6]
 command = "test command"
 action = { command = "test command", xxx = "yyy" }
 
-[plugin.metrics.3.4]
+[plugin.metrics.incorrect7.incorrect8]
 command = "test command"
 
-[plugin.checks.1]
+[plugin.checks.incorrect9]
 command = "test command"
 action = { command = "test command", user = "test user", en = { TEST_KEY = "VALUE_1" } }
 `
@@ -70,14 +70,14 @@ func TestValidateConfig(t *testing.T) {
 	wantUnexpectedKey := []UnexpectedKey{
 		{"filesystems.use_mntpoint", "filesystems.use_mountpoint"},
 		{"foobar", ""},
-		{"plugin.check.1", "plugin.checks.1"},
-		{"plugin.check.2", "plugin.checks.2"},
-		{"plugin.checks.1.action.en", "plugin.checks.1.action.env"},
+		{"plugin.check.incorrect2", "plugin.checks.incorrect2"},
+		{"plugin.check.incorrect3", "plugin.checks.incorrect3"},
+		{"plugin.checks.incorrect9.action.en", "plugin.checks.incorrect9.action.env"},
 		{"plugin.foo.bar", "plugin.metrics.bar"},
-		{"plugin.metric.1", "plugin.metrics.1"},
-		{"plugin.metrics.1.action.use", "plugin.metrics.1.action.user"},
-		{"plugin.metrics.2.action.xxx", ""},
-		{"plugin.metrics.3.4", ""},
+		{"plugin.metric.incorrect1", "plugin.metrics.incorrect1"},
+		{"plugin.metrics.incorrect5.action.use", "plugin.metrics.incorrect5.action.user"},
+		{"plugin.metrics.incorrect6.action.xxx", ""},
+		{"plugin.metrics.incorrect7.incorrect8", ""},
 		{"plugins", "plugin"},
 		{"podfile", "pidfile"},
 	}
