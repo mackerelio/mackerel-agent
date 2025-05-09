5bfb17cdd2fdb37a316ab389264dd86a1ae4d311
yseto <yseto@users.noreply.github.com>
2025-05-09 22:33:07 +0900

    remove CURRENT_REVISION, replace read VERSION from git.

--- build-k.bat
+++ build-k.bat
@@ -1,3 +1,1 @@
-FOR /F "usebackq" %%w IN (`git rev-parse --short HEAD`) DO SET COMMIT=%%w
-
-go build -o build/mackerel-agent-kcps.exe -ldflags="-X main.gitcommit=%COMMIT% -X github.com/mackerelio/mackerel-agent/config.apibase=http://198.18.0.16 " github.com/mackerelio/mackerel-agent
+go build -o build/mackerel-agent-kcps.exe -ldflags="-X github.com/mackerelio/mackerel-agent/config.apibase=http://198.18.0.16 " github.com/mackerelio/mackerel-agent
